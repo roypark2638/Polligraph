c78db3f617964504f65ec2c0c8359919a28f6cf5
Roy Park <roypark2638@gmail.com>
2021-03-01 23:54:02 -0800

    Firebase and User Interface environment setup.

---
+++ Polligraph/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
