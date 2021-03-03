77c1d64f023ec6fa173d0b2d1fb2012e84f1beae
Roy Park <roypark2638@gmail.com>
2021-03-02 18:52:54 -0800

    Created Extensions File implemented static methods, displayLoading and removeLoading Created Helper File and implemented static method, errorAlert

---
+++ Polligraph/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/2/21.
+//
+
+import Foundation
+import UIKit
+
+struct UIViewController {
+    // create the loading effect
+    static func displayLoading(withView: UIView) -> UIView {
+        
+        let spinnerView = UIView.init(frame: withView.bounds)
+        // we want this spinner background view to be invisible
+        spinnerView.backgroundColor = UIColor.clear
+        
+        let activityIndicator = UIActivityIndicatorView.init(style: .medium)
+        
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(activityIndicator)
+            withView.addSubview(spinnerView)
+        }
+        
+        return spinnerView
+    }
+    
+    // remove the loading effect
+    static func removeLoading(spinner: UIView) {
+        
+        DispatchQueue.main.async {
+            spinner.removeFromSuperview()
+        }
+    }
+}
---
+++ Polligraph/Helper.swift
@@ -0,0 +1,23 @@
+//
+//  Helper.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/2/21.
+//
+
+import Foundation
+import UIKit
+
+struct Helper {
+    
+    static func errorAlert(title: String, message: String) -> UIAlertController {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Okay", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(okAction)
+        return alert
+    }
+}
