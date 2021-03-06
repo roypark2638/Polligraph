e218af4c6aab85b6eea5e2579d7de848578ec5aa
Roy Park <roypark2638@gmail.com>
2021-03-06 14:35:56 -0800

    Check user's sign-in status

--- Polligraph/Controller/Core/HomeViewController.swift
+++ Polligraph/Controller/Core/HomeViewController.swift
@@ -6,24 +6,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        // check auth status
+        handleNotAuthenticated()
+    }
+    
+    private func handleNotAuthenticated() {
+        if Auth.auth().currentUser == nil {
+            // show singin page
+            let signinVC = SignInViewController()
+            // set the signinVC fullscreen so that user can't swipe the page away
+            signinVC.modalPresentationStyle = .fullScreen
+            
+            present(signinVC, animated: true, completion: nil)
+        }
+        else {
+            
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
