608d4feb768f4bf1aee5b3424e73e717e4cf922b
Roy Park <roypark2638@gmail.com>
2021-03-07 15:03:26 -0800

    Add basic SignIn Page UI and methods

--- Polligraph/Controller/Other/Onboarding/SignInViewController.swift
+++ Polligraph/Controller/Other/Onboarding/SignInViewController.swift
@@ -10,76 +10,187 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
-    private let emailTextField: UITextField = {
-        return UITextField()
+    struct Constants {
+        static let cornerRaius = CGFloat(10.0)
+    }
+    
+    // Create aumonyous closures
+    private let mainTitle: UILabel = {
+        let label = UILabel()
+        label.text = "polligraph"
+        label.font = label.font.withSize(62)
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.backgroundColor = .red
+        return label
+    }()
+
+    private let subTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Pick your side."
+        label.font = label.font.withSize(24)
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 1
+        label.textAlignment = .right
+        label.backgroundColor = .blue
+        
+        return label
     }()
     
-    private let passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.isSecureTextEntry = true        
-        return textField
+    private let emailAddressField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Email Address"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = Constants.cornerRaius
+        field.layer.masksToBounds = true
+        return field
     }()
     
-    private let signInButton: UITextField = {
-        return UITextField()
-    }()
-    
-    private let CreateAccountButton: UITextField = {
-        return UITextField()
-    }()
-    
-    private let headerView: UIView = {
-        return UIView()
-    }()
-    
-    private let termsButton: UIButton = {
-        return UIButton()
-    }()
-    
-    private let privacyButton: UIButton = {
-        return UIButton()
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Password"
+        field.isSecureTextEntry = true
+        field.returnKeyType = .continue
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = Constants.cornerRaius
+        field.layer.masksToBounds = true
+        return field
     }()
         
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = Constants.cornerRaius
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Forgot Password", for: .normal)
+        button.setTitleColor(.black , for: .normal)
+        button.backgroundColor = .systemBackground
+        button.layer.cornerRadius = Constants.cornerRaius
+        return button
+    }()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        self.navigationController?.navigationBar.backItem?.title = ""
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        addSubviews()
-        title = "Sign In Page"
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        signInButton.addTarget(
+            self,
+            action: #selector(didTapSignIn),
+            for: .touchUpInside)
+        
+        forgotPasswordButton.addTarget(
+            self,
+            action: #selector(didTapforgotPassword),
+            for: .touchUpInside)
+        
+        addSubviews()
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        
-//        // assign frames
-//        headerView.frame = CGRect(x: 0,
-//                                  y: view.safeAreaInsets.top,
-//                                  width: view.width,
-//                                  height: 200)
-//    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        mainTitle.frame = CGRect(
+            x: 70,
+            y: view.safeAreaInsets.top + 70,
+            width: view.width - 140,
+            height: 80)
+        
+        subTitle.frame = CGRect(
+            x: 70,
+            y: mainTitle.bottom + 10,
+            width: view.width - 140,
+            height: 35)
+        
+        emailAddressField.frame = CGRect(
+            x: 35,
+            y: subTitle.bottom + 120,
+            width: view.width - 70,
+            height: 46)
+        
+        passwordField.frame = CGRect(
+            x: 35,
+            y: emailAddressField.bottom + 20,
+            width: view.width - 70,
+            height: 46)
+
+        signInButton.frame = CGRect(
+            x: 35,
+            y: passwordField.bottom + 40,
+            width: view.width - 70,
+            height: 46)
+
+        forgotPasswordButton.frame = CGRect(
+            x: 35,
+            y: signInButton.bottom + 15,
+            width: view.width - 70,
+            height: 46)
+        
+    }
     
     private func addSubviews() {
-        view.addSubview(emailTextField)
-        view.addSubview(passwordTextField)
+        view.addSubview(mainTitle)
+        view.addSubview(subTitle)
+        view.addSubview(emailAddressField)
+        view.addSubview(passwordField)
         view.addSubview(signInButton)
-        view.addSubview(CreateAccountButton)
-        view.addSubview(headerView)
-        view.addSubview(termsButton)
-        view.addSubview(privacyButton)
-        
+        view.addSubview(forgotPasswordButton)
     }
     
-    @objc private func didTouchSignInButton() {
-        
+    private func createBackButton() -> UIButton {
+        let backButtonImage = UIImage(systemName: "arrow.backward")
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(backButtonImage, for: .normal)
+        backButton.tintColor = .black
+        backButton.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        return backButton
     }
     
-    @objc private func didTouchCreateAccountButton() {
-        
+    @objc private func didTapSignIn() {
+        let vc = RegistrationViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.modalPresentationStyle = .fullScreen
+        nav.navigationBar.prefersLargeTitles = true
+        vc.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: createBackButton())
+        present(nav, animated: true, completion: nil)
     }
+    
+    
+    // this needs to be changed to navigate to the tabBar controller if auth passes
+    @objc private func didTapforgotPassword() {
+        let vc = SignInViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.modalPresentationStyle = .fullScreen
+        nav.navigationBar.prefersLargeTitles = true
+        vc.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: createBackButton())
+        present(nav, animated: true, completion: nil)
+    }
+    
+    @objc private func backAction() {
+        dismiss(animated: true, completion: nil)
+    }
+}
     
 //    @IBAction func signinButtonPressed(_ sender: UIButton) {
 //        guard let email = emailTextField.text else { return }
@@ -130,5 +241,5 @@ class SignInViewController: UIViewController {
 //    }
     
     
-}
+//}
 
