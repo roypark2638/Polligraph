642095c094f883f80f038438480fce5648f29ad0
Roy Park <roypark2638@gmail.com>
2021-03-09 20:17:24 -0800

    Created initial model for settings menu

---
+++ Polligraph/Models/SettingsModels.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsModels.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/9/21.
+//
+
+import Foundation
+
+struct SettingsCellModel {
+    let title: String
+    let handler: (() -> Void)
+}
