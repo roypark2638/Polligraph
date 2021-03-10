31d2f51310f506c09ec4735c4d84cb6ca567db97
Roy Park <roypark2638@gmail.com>
2021-03-09 20:15:33 -0800

    Created a settings button in the view

--- Polligraph/Controller/Core/ProfileViewController.swift
+++ Polligraph/Controller/Core/ProfileViewController.swift
@@ -13,9 +13,22 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        
+        configureNavigationBar()
     }
     
-
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            style: .done,
+            target: self,
+            action: #selector(didTapSettingsButton))
+    }
  
+    @objc private func didTapSettingsButton() {
+        let vc = SettingsViewController()
+        vc.title = "Settings"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
