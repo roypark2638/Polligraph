2320cca34652bbdc17d34d9a1215634e00c24ef4
Roy Park <roypark2638@gmail.com>
2021-03-11 01:29:48 -0800

    Added SettignsCellModel

---
+++ Polligraph/Models/SettingsCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsCellModel.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/10/21.
+//
+
+import Foundation
+
+struct SettingsCellModel{
+    let title: String
+    let handler: (() -> Void)
+}
+
