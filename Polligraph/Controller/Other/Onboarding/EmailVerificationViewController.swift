706bc27aa43709b2949a723290919636ca4ff259
Roy Park <roypark2638@gmail.com>
2021-03-22 13:37:13 -0700

    Added lineSpacing and setUp the final constraints

--- Polligraph/Controller/Other/Onboarding/EmailVerificationViewController.swift
+++ Polligraph/Controller/Other/Onboarding/EmailVerificationViewController.swift
@@ -24,26 +24,26 @@ class EmailVerificationViewController: UIViewController {
         return label
     }()
     
-    private let mailView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "Mail UI")
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFit
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-//
-//    private let mailView: UIView = {
-//        let mail = UIView()
-//        mail.clipsToBounds = true
-//        let mailImageView = UIImageView(image: UIImage(named: "Mail UI"))
-//        mailImageView.contentMode = .scaleAspectFit
-//        mailImageView.layer.masksToBounds = true
-//        mail.layer.masksToBounds = true
-//
-//        mail.addSubview(mailImageView)
-//        return mail
+//    private let mailView: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.image = UIImage(named: "Mail UI")
+//        imageView.clipsToBounds = true
+//        imageView.contentMode = .scaleAspectFit
+//        imageView.layer.masksToBounds = true
+//        return imageView
 //    }()
+
+    private let mailView: UIView = {
+        let mail = UIView()
+        mail.clipsToBounds = true
+        let mailImageView = UIImageView(image: UIImage(named: "Mail UI"))
+        mailImageView.contentMode = .scaleAspectFit
+        mailImageView.layer.masksToBounds = true
+        mail.layer.masksToBounds = true
+
+        mail.addSubview(mailImageView)
+        return mail
+    }()
     
     private let subLabel1: UILabel = {
         let label = UILabel()
@@ -65,8 +65,12 @@ class EmailVerificationViewController: UIViewController {
         let email = Auth.auth().currentUser?.email ?? ""
         let boldFont = [NSAttributedString.Key.font : UIFont(name: "Roboto-Bold", size: 16)]
         let attributedString = NSMutableAttributedString(string: email, attributes: boldFont as [NSAttributedString.Key : Any])
-                
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        paragraphStyle.lineSpacing = 3
+        
         regularString.append(attributedString)
+        regularString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, regularString.length))
         label.attributedText = regularString
         label.textAlignment = .center
         label.numberOfLines = 0
@@ -78,13 +82,16 @@ class EmailVerificationViewController: UIViewController {
     
     private let subLabel3: UILabel = {
         let label = UILabel()
-        label.text = "Make sure to check your Spam or Junk folder if you can't find the verification email."
+        let attributedString = NSMutableAttributedString(string: "Make sure to check your Spam or Junk folder if you can't find the verification email.")
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 3
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+        label.attributedText = attributedString
         label.textColor = .lightGray
         label.font = UIFont(name: "Roboto-Regular", size: 16)
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.sizeToFit()
-//        label.adjustsFontSizeToFitWidth = true
+        label.adjustsFontSizeToFitWidth = true
         return label
     }()
     
@@ -101,16 +108,8 @@ class EmailVerificationViewController: UIViewController {
     }()
     
     private let signInButton = AuthButton(type: .black, title: "Sign In")
-//    private let signInButton: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("Sign In", for: .normal)
-//        button.backgroundColor = .systemBackground
-//        button.setTitleColor(.label, for: .normal)
-//        button.titleLabel?.font = UIFont(name: "Roboto-Bold", size: 16)
-//        button.titleLabel?.textAlignment = .center
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        return button
-//    }()
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -123,7 +122,7 @@ class EmailVerificationViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         view.backgroundColor = .systemBackground
-        addLayouts()
+        setUpLayout()
         
     }
     
@@ -137,9 +136,8 @@ class EmailVerificationViewController: UIViewController {
         view.addSubview(signInButton)
     }
     
-    private func addLayouts() {
-        let imageWidth = view.width/2.8
-        let imageHeight = view.width/5.6
+    private func setUpLayout() {
+
         
         titleLabel.frame = CGRect(
             x: 24,
@@ -148,12 +146,14 @@ class EmailVerificationViewController: UIViewController {
             height: 33
         )
         
+        let imageWidth = view.width/2.8
+        let imageHeight = view.width/5.6
+        
         mailView.frame = CGRect(
             x: (view.width-imageWidth)/2,
             y: titleLabel.bottom + 32,
             width: imageWidth,
             height: imageHeight
-
         )
         
         subLabel1.frame = CGRect(
@@ -167,34 +167,32 @@ class EmailVerificationViewController: UIViewController {
             x: 24,
             y: subLabel1.bottom + 40,
             width: view.width - 48,
-            height: 22
+            height: 44
         )
-        
-        subLabel3.sizeToFit()
-        let titleLabelSize = subLabel3.sizeThatFits(CGSize(width: imageWidth, height: view.height))
-
+    
         subLabel3.frame = CGRect(
             x: 24,
             y: subLabel2.bottom + 40,
             width: view.width - 48,
-            height: titleLabelSize.height
+            height: 44
         )
 
         subLabel4.frame = CGRect(
             x: 24,
-            y: subLabel3.bottom + 40,
+            y: subLabel3.bottom + 70,
             width: view.width - 48,
             height: 22
         )
 
         signInButton.frame = CGRect(
             x: 24,
-            y: subLabel4.bottom + 40,
+            y: subLabel4.bottom + 32,
             width: view.width - 48,
             height: 50
         )
     }
     
+    // MARK: - Objc Methods
     
     @objc private func didTouchSignIn() {
         let vc = OnboardingViewController()
