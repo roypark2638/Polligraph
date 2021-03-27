5ca24a69d2592ff059e3c67d5700d86a3f0e6817
Roy Park <roypark2638@gmail.com>
2021-03-27 04:42:26 -0700

    Setup ProfileHeaderViewModel

---
+++ Polligraph/ViewModels/ProfileHeaderViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileHeaderViewModel.swift
+//  Polligraph
+//
+//  Created by Roy Park on 3/27/21.
+//
+
+import Foundation
+
+struct ProfileHeaderViewModel {
+    let profileImageURL: URL?
+    let isFollowing: Bool?
+    let followerCount: Int
+    let pollsCount: Int
+}
+
+
