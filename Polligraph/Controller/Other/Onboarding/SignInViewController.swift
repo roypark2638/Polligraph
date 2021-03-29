b31d62547f154c2298638b4a5d745a86e038bfa1
Roy Park <roypark2638@gmail.com>
2021-03-28 21:20:52 -0700

    Implemented Sign In With Apple

--- Polligraph/Controller/Other/Onboarding/SignInViewController.swift
+++ Polligraph/Controller/Other/Onboarding/SignInViewController.swift
@@ -7,33 +7,9 @@
 import GoogleSignIn
 import UIKit
 import FirebaseAuth
+import AuthenticationServices
 
-class SignInViewController: UIViewController, GIDSignInDelegate {
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(
-            withIDToken: authentication.idToken,
-            accessToken: authentication.accessToken)
-        
-        Auth.auth().signIn(with: credential) { [weak self] (authResult, error) in
-            guard authResult != nil, error == nil else {
-                // error
-                return
-            }
-            DispatchQueue.main.async {
-                let vc = TabBarViewController()
-                vc.modalPresentationStyle = .fullScreen
-                self?.present(vc, animated: true, completion: nil)
-            }
-            
-        }
-    }
-    
-    
+class SignInViewController: UIViewController {
     // MARK: - Properties
 
     struct Constants {
@@ -345,9 +321,31 @@ class SignInViewController: UIViewController, GIDSignInDelegate {
         dismiss(animated: true, completion: nil)
     }
     
+    fileprivate var currentNonce: String?
+
     private func performAppleSignIn() {
+        let request = createAppleIDRequest()
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        
+        authorizationController.performRequests()
         
     }
+    
+    private func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+        return request
+    }
+    
+    
 }
 
 // MARK: - UITextFieldDelegate
@@ -361,6 +359,85 @@ extension SignInViewController: UITextFieldDelegate {
         }
         return true
     }
+}
+
+// MARK: - GIDSignInDelegate
+extension SignInViewController: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(
+            withIDToken: authentication.idToken,
+            accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { [weak self] (authResult, error) in
+            guard authResult != nil, error == nil else {
+                // error
+                return
+            }
+            print("Successfully login with Google")
+            DispatchQueue.main.async {
+                let vc = TabBarViewController()
+                vc.modalPresentationStyle = .fullScreen
+                self?.present(vc, animated: true, completion: nil)
+            }
+            
+        }
+    }
+}
+
+extension SignInViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+          guard let nonce = currentNonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+          }
+          guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Unable to fetch identity token")
+            return
+          }
+          guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return
+          }
+          // Initialize a Firebase credential.
+          let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                    idToken: idTokenString,
+                                                    rawNonce: nonce)
+          // Sign in with Firebase.
+          Auth.auth().signIn(with: credential) { (authResult, error) in
+            if error != nil {
+              // Error. If error.code == .MissingOrInvalidNonce, make sure
+              // you're sending the SHA256-hashed nonce as a hex string with
+              // your request to Apple.
+              print(error!.localizedDescription)
+              return
+            }
+            // User is signed in to Firebase with Apple.
+            print("Successfully login with Apple")
+            DispatchQueue.main.async {
+                let vc = TabBarViewController()
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true, completion: nil)
+            }
+          }
+        }
+      }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("failed to log in!")
+    }
+}
+
+extension SignInViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
+    
 }
     
 //    @IBAction func signinButtonPressed(_ sender: UIButton) {
@@ -414,3 +491,67 @@ extension SignInViewController: UITextFieldDelegate {
     
 //}
 
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+
+import CryptoKit
+
+// Unhashed nonce.
+fileprivate var currentNonce: String?
+
+@available(iOS 13, *)
+func startSignInWithAppleFlow() {
+  let nonce = randomNonceString()
+  currentNonce = nonce
+  let appleIDProvider = ASAuthorizationAppleIDProvider()
+  let request = appleIDProvider.createRequest()
+  request.requestedScopes = [.fullName, .email]
+  request.nonce = sha256(nonce)
+
+  let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+//  authorizationController.delegate = self
+//  authorizationController.presentationContextProvider = self
+  authorizationController.performRequests()
+}
+
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+  let inputData = Data(input.utf8)
+  let hashedData = SHA256.hash(data: inputData)
+  let hashString = hashedData.compactMap {
+    return String(format: "%02x", $0)
+  }.joined()
+
+  return hashString
+}
