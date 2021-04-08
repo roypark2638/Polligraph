3f03bbe764baac6f15a477815548db69b4808f99
Roy Park <roypark2638@gmail.com>
2021-04-08 03:09:22 -0700

    Create Notification Cell files

---
+++ Polligraph/Views/Notification/NotificationPostCommentCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationPostCommentCollectionViewCell.swift
+//  Polligraph
+//
+//  Created by Roy Park on 4/8/21.
+//
+
+import UIKit
+
+class NotificationPostCommentCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Polligraph/Views/Notification/NotificationPostCommentLikeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationPostCommentLikeCollectionViewCell.swift
+//  Polligraph
+//
+//  Created by Roy Park on 4/8/21.
+//
+
+import UIKit
+
+class NotificationPostCommentLikeCollectionViewCell: UICollectionViewCell {
+    
+}
