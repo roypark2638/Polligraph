013637f2b304d20571aa18a11dcd000f79548528
Roy Park <roypark2638@gmail.com>
2021-04-27 16:29:37 -0700

    setup for camera/library vc

---
+++ Polligraph/Views/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  Polligraph
+//
+//  Created by Roy Park on 4/27/21.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+
+}
---
+++ Polligraph/Views/LibraryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryViewController.swift
+//  Polligraph
+//
+//  Created by Roy Park on 4/27/21.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+        
+    }
+
+
+}
