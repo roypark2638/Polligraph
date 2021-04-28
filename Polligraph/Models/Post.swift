826f2a52b4a7efd8d4716f2fda3895d1e5a0a186
Roy Park <roypark2638@gmail.com>
2021-04-27 18:27:25 -0700

    Added Post Model

---
+++ Polligraph/Models/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  Polligraph
+//
+//  Created by Roy Park on 4/27/21.
+//
+
+import Foundation
+
+struct Post: Codable {
+    // id will point to the storage reference where the photo is
+    let id: String
+    let caption: String?
+    let postedDate: String
+    let postURLString: String
+    var likers: [String]
+    
+    var storageReference: String? {
+        guard let username = UserDefaults.standard.string(forKey: "username") else { return nil }
+        
+        return "\(username)/posts/\(id).png"
+    }
+}
