510f04580a910f848f3af6abfd3dfa5aa3df0cae
Roy Park <roypark2638@gmail.com>
2021-05-05 18:01:44 -0700

    Cleaned AppDelegate

--- Polligraph/Resources/AppDelegate.swift
+++ Polligraph/Resources/AppDelegate.swift
@@ -45,27 +45,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: Google SignIn Methods
     
-//    func application(_ application: UIApplication,
-//                     open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-//        if GIDSignIn.sharedInstance().handle(url) {
-//            return true
-//        }
-//        else if ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation) {
-//            return true
-//        }
-//        return false
-//    }
-    
     @available(iOS 9.0, *)
     func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
       -> Bool {
-//        if GIDSignIn.sharedInstance().handle(url) {
-//            return true
-//        }
-//        else if ApplicationDelegate.shared.application(app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation]) {
-//            return true
-//        }
-//        return false
         
         ApplicationDelegate.shared.application(
             application,
@@ -79,15 +61,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
     }
 
 
